a22a57a7e3d91041d0f19660653c55f95704585c
Michael <mlandryviolin@gmail.com>
2020-09-19 14:18:36 -0400

    a broken mergesort impl

---
+++ datastructure-algo-implementations/mergesort.js
@@ -0,0 +1,26 @@
+//its a recursive implementation, so solve the base case
+function mergeSort (arr)
+{
+  if (arr.length === 0 || arr.length === 1) return arr
+  const result = []
+  const half = Math.ceil(arr.length / 2);
+  const leftP = mergeSort(arr.splice(0, half))
+  const rightP = mergeSort(arr.splice(-half))
+
+  while (leftP.length || rightP.length)
+  {
+    if(leftP.length===0) {
+      result.push(...rightP)
+      break
+    }
+    if(rightP.length===0) {
+      result.push(...leftP)
+      break
+    }
+    const smaller = (leftP[0] < rightP[0])  ? leftP.pop() : rightP.pop()
+    result.push(smaller)
+  }
+  return result
+}
+
+console.log('test', mergeSort([3,2,1,55]))
